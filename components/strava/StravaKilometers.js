82daf2783d6255f5fe9a506ca1951487e5566d24
Ben Carlson <bjcarlson42@outlook.com>
2021-01-22 13:33:49 -0500

    Fixed NaN error.

--- components/strava/StravaKilometers.js
+++ components/strava/StravaKilometers.js
@@ -3,7 +3,11 @@ import fetcher from '../../lib/fetcher'
 
 const StravaKilometers = () => {
     const { data } = useSWR('/api/strava', fetcher)
-    const distancia = (data?.distance / 1000).toFixed(0)
+    const distancia = data
+
+    // conversions
+    const distanceConverted = (distancia / 1000).toFixed(0)
+
     return (distancia ? distancia : '---')
 }
 
