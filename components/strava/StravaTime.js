0144da4d9ddcd2c2280cd067142a3d60435ad7c9
Luis Martínez <ofluis@outlook.com>
2021-01-23 17:26:00 +0100

    Update StravaTime.js

--- components/strava/StravaTime.js
+++ components/strava/StravaTime.js
@@ -1,10 +1,12 @@
 import useSWR from 'swr'
 import fetcher from '../../lib/fetcher'
+import { Loader } from '../Loader'
 
 const StravaTime = () => {
     const { data } = useSWR('/api/strava', fetcher)
-    const tiempo = (data?.time * 0.000277778).toFixed(0)
-    return (tiempo ? tiempo : '---') 
+    const tiempo = data
+    const timeConverted = (data?.time * 0.000277778).toFixed(0)
+    return (tiempo ? timeConverted : <Loader />) 
 }
 
 export default StravaTime
