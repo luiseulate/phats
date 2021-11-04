87c7c16bb13fab42759d6bb771f445e1ca4add1e
Luis Martínez <ofluis@outlook.com>
2021-11-04 20:47:35 +0100

    Update manifesto.js

--- pages/manifesto.js
+++ pages/manifesto.js
@@ -1,14 +1,11 @@
 export default function Home() { return (
   
 <div id="wrapper">
-<div id="bg"></div>
-<div id="overlay"></div>
-<div id="main">
-<header id="header">
-<h1>Strava Phats</h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla semper vel nisi ac tristique. Nullam varius nibh magna, vitae convallis nisi mattis vitae. Pellentesque nibh risus, viverra id risus non, consequat tincidunt lectus. In luctus risus non quam tristique scelerisque quis sit amet tellus. In id metus metus. Ut euismod euismod urna, at elementum risus convallis nec. Vestibulum vestibulum fringilla semper.</p>
-</header>
-</div>
+    <div id="bg"></div>
+    <div id="overlay"></div>
+    <div id="main">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla semper vel nisi ac tristique. Nullam varius nibh magna, vitae convallis nisi mattis vitae. Pellentesque nibh risus, viverra id risus non, consequat tincidunt lectus. In luctus risus non quam tristique scelerisque quis sit amet tellus. In id metus metus. Ut euismod euismod urna, at elementum risus convallis nec. Vestibulum vestibulum fringilla semper.</p>
+    </div>
 </div>
 
 )}
