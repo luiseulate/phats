a9f649a5e10201b4f5e418827e8bf6835dd6bae0
Luis Martínez <ofluis@outlook.com>
2021-11-04 20:28:50 +0100

    Create Header.js

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+export const Header = () => {
+    return (
+        <header id="header">
+				  <h1>Strava Phats</h1>
+					<p>Physical activity stats from Strava</p>
+					<nav>
+					  <ul>
+							<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+						</ul>
+					</nav>
+			  </header>
+    )
+}
