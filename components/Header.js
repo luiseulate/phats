a39a652324e3bb1fbb2ce91a95e34087cd7449d0
Luis Martínez <ofluis@outlook.com>
2021-11-04 20:43:43 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,8 @@
 export const Header = () => {
     return (
         <header id="header">
-				  <h1>Strava Phats</h1>
-					<p>Physical activity stats from Strava</p>
-					<nav>
-					  <ul>
-							<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
-						</ul>
-					</nav>
-			  </header>
+		<h1>Strava Phats</h1>
+		<p>Physical activity stats from Strava</p>
+	</header>
     )
 }
