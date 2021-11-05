1029111eca8c9f338099a527e60c90979048dd06
Luis Martínez <ofluis@outlook.com>
2021-11-05 10:13:07 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ export const Header = () => {
     return (
         <header id="header">
 		<h1>Strava Stats</h1>
-	    	<a href="https://www.strava.com/athletes/luiseulate"><img src="/static/assets/css/images/strava-icon.svg" alt="Strava Stats" width="32" height="32"/></a>
+	    	<p><a href="https://www.strava.com/athletes/luiseulate"><img src="/static/assets/css/images/strava-icon.svg" alt="Strava Stats" width="32" height="32"/></a></p>
 	</header>
     )
 }
