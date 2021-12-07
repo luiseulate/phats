99f4d3f191a90a919c2298b3497c976604ff41bd
Luis Martínez <ofluis@outlook.com>
2021-12-07 13:27:53 +0100

    Delete Header.js

--- components/Header.js
+++
@@ -1,8 +0,0 @@
-export const Header = () => {
-    return (
-        <header id="header">
-		<h1>Strava Stats</h1>
-	    	<p><a href="https://www.strava.com/athletes/luiseulate"><img src="/static/assets/css/images/strava-icon.svg" alt="Strava Stats" width="32" height="32"/></a></p>
-	</header>
-    )
-}
