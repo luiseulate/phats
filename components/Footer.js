77881c34130a300123ff552a6a3510faac3fda89
Luis Martínez <ofluis@outlook.com>
2023-04-29 13:06:55 +0200

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ export const Footer = () => {
     return (
         <footer id="footer">
             <span className="copyright">
-                <a href="https://luiseulate.com" target="_blank">by Luis Martínez</a>. Powered by <a href="https://developers.strava.com" target="_blank">Strava API</a> - <a href="https://github.com/luiseulate/Phats" target="_blank">GitHub</a> - <a href="https://vercel.com" target="_blank">Vercel</a>. Special thanks to <a href="https://benjamincarlson.io/" target="_blank">Benjamin Carlson</a>
+                Powered by <a href="https://developers.strava.com" target="_blank">Strava API</a>. Thanks to <a href="https://benjamincarlson.io/" target="_blank">Benjamin Carlson</a>
             </span>
         </footer>
     )
